de79258e6799f6bd3053fb178f56808e0eb6f7bb
Toan Le <thanhtoanhcm1602@gmai.com>
2023-08-29 11:01:10 +0700

    - NextJS: 02-06 SSG getStaticProps with typescript.

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,37 @@
+import { GetStaticProps, GetStaticPropsContext } from "next";
 import React from "react";
 
-export interface PostListPageProps {}
+export interface PostListPageProps {
+  posts: any[];
+}
 
-export default function PostListPage(props: PostListPageProps) {
+export default function PostListPage({ posts }: PostListPageProps) {
+  // Client side.
+  // posts get from NEXT_DATA after pre-render html.
+
   return (
     <div>
       <h1> Post list page</h1>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (context: GetStaticPropsContext) => {
+  // Server side.
+  // build-time.
+
+  const response = await fetch("https://js-post-api.herokuapp.com/api/posts?_page=1");
+  const data = await response.json();
+
+  // console.log(data);
+
+  return {
+    props: { posts: data.data.map((x: any) => ({ id: x.id, title: x.title })) },
+  };
+};
